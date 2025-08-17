9c64ae96cce861cc8e9e50ef3065fe10128e8367
matt <matheusmiriani@hotmail.com>
2025-08-17 16:04:22 -0300

    Ajustes no backend

--- server/src/main/java/com/example/scheduler/domain/entity/Subject.java
+++ server/src/main/java/com/example/scheduler/domain/entity/Subject.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "subject")
+//@Entity(name = "subject")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
